735539b1fa287e7df26dd4a121ec8cc76cd6b606
Maicon Santos <maiconfsanto@gmail.com>
2020-06-18 11:15:10 -0300

    config

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,9 @@
-import express from 'express';
+import express, { response, request } from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    console.log('Listagem de Usuários');
-    
-    response.json([
-        'diego',
-        'cleinton',
-        'robson',
-        'Maicon'
-    ]);
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
